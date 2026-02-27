f83416b7d670f7f83b44684c5d5f029b7fef0bc1
Deepanjan Biswas <deepanjanxyz@gmail.com>
2026-02-27 17:11:34 +0530

    Create app.c

---
+++ app.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("=========================================\n");
+    printf("   DEEPANJAN-ER PROTHOM C APP READY!    \n");
+    printf("=========================================\n");
+    printf("Hello World! Eita tor C diye banano app.\n");
+    printf("=========================================\n");
+    return 0;
+}
